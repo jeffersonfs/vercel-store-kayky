ab24b63d234db52678a377871179ddf3cefb15e8
Jefferson F. Silva <jefferson.font@gmail.com>
2023-09-25 22:04:45 -0300

    Bug fixed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,15 +7,6 @@ const jogoData: Prisma.JogoCreateInput[] = [
 
 async function main() {
     console.log(`Start seeding ...`);
-    for (const  pjogo of jogoData) {
-        // create jogo if not exists
-        const jogo = await prisma.jogo.upsert({
-            where: { id: pjogo.id },
-            create: pjogo,
-            update: {},
-        });
-        console.log(`Upserted Pet with id: ${jogo.id}`);
-    }
     console.log(`Seeding finished.`);
 }
 
